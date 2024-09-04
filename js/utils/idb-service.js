70ff6ed36060de3638f5f7ff358c6745153adb2a
Kruiser8 <kruiser.twitch@gmail.com>
2024-09-03 22:16:56 -0400

    update idb-keyval to 6.2.1

--- js/utils/idb-service.js
+++ js/utils/idb-service.js
@@ -2,18 +2,37 @@
  * IDBService is a proxy for interacting with IndexedDB through idbKeyval.
  */
  class IDBService {
+
+  /**
+   * Waits for the idbKeyval global module to be loaded.
+   */
+  static async awaitModuleLoad() {
+    if (typeof idbKeyval !== 'undefined') {
+      return;
+    }
+
+    return await new Promise((resolve) => {
+      setTimeout(async () => { 
+        await IDBService.awaitModuleLoad();
+        resolve();
+      }, 100);
+    });
+  }
+
   /**
    * Retrieve a value from IndexedDB by the key.
    * @param {string} key id of the value to retrieve
    */
   static async get(key) {
+    await IDBService.awaitModuleLoad();
     return await idbKeyval.get(key);
   }
 
   /**
-   * Retrieve all entries from IndexedDB.
+   * Retrieve all keys from IndexedDB.
    */
   static async keys() {
+    await IDBService.awaitModuleLoad();
     return await idbKeyval.keys();
   }
 
@@ -23,6 +42,7 @@
    * @param {object} value object to store in the entry
    */
   static async set(key, value) {
+    await IDBService.awaitModuleLoad();
     await idbKeyval.set(key, value);
   }
 
@@ -31,6 +51,7 @@
    * @param {string} key id of the value to delete
    */
   static async delete(key) {
+    await IDBService.awaitModuleLoad();
     idbKeyval.del(key);
   }
 
@@ -38,6 +59,7 @@
    * Clear all entries from IndexedDB.
    */
   static async clear() {
+    await IDBService.awaitModuleLoad();
     idbKeyval.clear();
   }
 }
